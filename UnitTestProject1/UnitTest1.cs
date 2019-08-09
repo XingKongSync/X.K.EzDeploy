2f725392d55b396c07a3773d7deb46bd04c522fb
Liuqi <liuqi@iseetech.com.cn>
2019-08-09 17:46:35 +0800

    修改测试用例

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestInstallService()
         {
-            Srvany.InstallService("XkedTestService", @"D:\Program Files\n2n\edge_v2.exe", "-r -a dhcp:0.0.0.0 -c xingkong -k xingkong -l 65.49.192.196:6900", "XkedTestServiceDisplayName", "This is a test Desc");
+            Srvany.InstallService("XkedTestService", @"D:\test.exe", "-s", "XkedTestServiceDisplayName", "This is a test Desc");
         }
 
         [TestMethod]
